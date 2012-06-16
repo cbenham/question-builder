8bffca1bae17add3ba3135f21523219680e079d3
Conrad Benham <cbenham@gmail.com>
2012-06-16 12:19:59 -0400

    Refactored lesson into an object and starting index counter at 0 instead of 1.

--- spec/lesson-spec.js
+++ spec/lesson-spec.js
@@ -9,7 +9,7 @@ describe("Lessons", function() {
         it("should clear the container of questions", function() {
             loadFixtures("question.html");
             $("#question-container").question("Arbitrary question", {expected: "Irrelevant", answer: "Unimportant"});
-            expect($("#question-container div")).toHaveId("question-1");
+            expect($("#question-container div")).toHaveId("question-0");
 
             $("#question-container").reset();
             
@@ -19,13 +19,13 @@ describe("Lessons", function() {
         it("should reset the counter", function() {
             loadFixtures("question.html");
             $("#question-container").question("Arbitrary question", {expected: "Irrelevant", answer: "Unimportant"});
-            expect($("#question-container div")).toHaveId("question-1");
+            expect($("#question-container div")).toHaveId("question-0");
             
             $("#question-container").reset();
             
             $("#question-container").question("Another question", {expected: "Irrelevant", answer: "Unimportant"});
-            expect($("#question-1").size()).toEqual(1);
-            expect($("#question-2").size()).toEqual(0);
+            expect($("#question-0").size()).toEqual(1);
+            expect($("#question-1").size()).toEqual(0);
         });
     });
     
@@ -33,13 +33,13 @@ describe("Lessons", function() {
         it("should show the question, expected answer and actual answer", function() {
             loadFixtures("question.html");
             $("#question-container").question("What is 1 + 4?", {expected: 5, answer: 0});
-            expect($("#question-container #question-1")).toHaveHtml("What is 1 + 4?<br>expected: 5<br>but got: 0");
+            expect($("#question-container #question-0")).toHaveHtml("What is 1 + 4?<br>expected: 5<br>but got: 0");
         });
         
         it("should be styled with a red bar", function() {
             loadFixtures("question.html");
             $("#question-container").question("What is 1 + 4?", { expected: 5, answer: 0 });
-            expect($("#question-1")).toHaveClass("incorrect-answer");
+            expect($("#question-0")).toHaveClass("incorrect-answer");
         });
     });
     
@@ -47,13 +47,13 @@ describe("Lessons", function() {
         it("should show only show the question and answer", function() {
             loadFixtures("question.html");
             $("#question-container").question("What is 3 + 5?", {expected: 8, answer: 8});
-            expect($("#question-1").html()).toBe("What is 3 + 5?<br>Correctly answered: 8");
+            expect($("#question-0").html()).toBe("What is 3 + 5?<br>Correctly answered: 8");
         });
         
         it("should be style with a green bar", function() {
             loadFixtures("question.html");
             $("#question-container").question("What is 3 + 5?", {expected: 8, answer: 8});
-            expect($("#question-1")).toHaveClass("correct-answer");
+            expect($("#question-0")).toHaveClass("correct-answer");
         });
     });
 });
